3e968c86816250e1572ce961ffe9b66f0de9d029
2009Parth <111286031+2009Parth@users.noreply.github.com>
2023-02-20 20:58:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function soundClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/0JIsnmYtX/model.json', modelReady);
+}
+
+
+
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+
+
+
+
+
+function gotResults(error, results)
+{
+    console.log(gotResults);
+}
